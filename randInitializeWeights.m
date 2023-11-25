edc8c5d98ba6cf19b81fc7f9f87cb5a4f989b1dd
Cristian18042 <65841018+Cristian18042@users.noreply.github.com>
2023-11-25 15:09:06 -0300

    randInitializeWeights.m

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -1,6 +1,6 @@
 function W = randInitializeWeights(L_in, L_out)
-%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
-%incoming connections and L_out outgoing connections
+% RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+% incoming connections and L_out outgoing connections
 %   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
 %   of a layer with L_in incoming connections and L_out outgoing 
 %   connections. 
