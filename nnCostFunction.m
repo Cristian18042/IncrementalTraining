748a354137896b5ba7cde5ee323458c3b519f501
Cristian18042 <65841018+Cristian18042@users.noreply.github.com>
2023-11-25 15:10:08 -0300

    nnCostFunction.m

--- nnCostFunction.m
+++ nnCostFunction.m
@@ -3,8 +3,8 @@ function [J,grad] = nnCostFunction(nn_params, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
                                    X, y, lambda)
-%NNCOSTFUNCTION Implements the neural network cost function for a two layer
-%neural network which performs classification
+% NNCOSTFUNCTION Implements the neural network cost function for a two layer
+% neural network which performs classification
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
@@ -12,7 +12,6 @@ function [J,grad] = nnCostFunction(nn_params, ...
 % 
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
-%
 
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
@@ -27,27 +26,27 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 X = [ones(m, 1) X];
-a1=X;
+a1 = X;
 
 z2 = a1*Theta1';
 a2 = sigmoid(z2);
 a2 = [ones(m, 1) a2];
 z3 = a2*Theta2';
 a3 = sigmoid(z3);
-h=a3;
-I=eye(num_labels);
-for i=1:size(y,1)
-   Y(i,:)=I(y(i),:); 
+h = a3;
+I = eye(num_labels);
+for i = 1:size(y,1)
+   Y(i,:) = I(y(i),:); 
 end
-% You need to return the following variables correctly 
+
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
-l1=log(h);
-l2=log(1-h);
-for i=1:m
+l1 = log(h);
+l2 = log(1-h);
+for i = 1:m
    acu = sum((-Y(i,:)*l1(i,:)' - ((1-Y(i,:))*l2(i,:)')));
-   J=J+acu;
+   J = J+acu;
    delta_3(i,:) = a3(i,:)-Y(i,:);
    delta_2(i,:) = delta_3(i,:)*Theta2(:,2:end).*sigmoidGradient(z2(i,:));
 end
@@ -55,22 +54,20 @@ Theta1_grad(:,1) = (Theta1_grad(:,1) + delta_2'*a1(:,1))/m;
 Theta2_grad(:,1) = (Theta2_grad(:,1) + delta_3'*a2(:,1))/m;
 Theta1_grad(:,2:end) = (Theta1_grad(:,2:end) + delta_2'*a1(:,2:end) + lambda*Theta1(:,2:end))/m;
 Theta2_grad(:,2:end) = (Theta2_grad(:,2:end) + delta_3'*a2(:,2:end) +lambda*Theta2(:,2:end))/m;
-J=(1/m)*J;
-t1=Theta1(:,2:end);
-t1=t1(:);
-t2=Theta2(:,2:end);
-t2=t2(:);
-s=lambda/(2*m)*(t1'*t1+t2'*t2);
-J=s+J;
+J = (1/m)*J;
+t1 = Theta1(:,2:end);
+t1 = t1(:);
+t2 = Theta2(:,2:end);
+t2 = t2(:);
+s = lambda/(2*m)*(t1'*t1+t2'*t2);
+J = s+J;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
 %               following parts.
 %
 % Part 1: Feedforward the neural network and return the cost in the
-%         variable J. After implementing Part 1, you can verify that your
-%         cost function computation is correct by verifying the cost
-%         computed in ex4.m
+%         variable J.
 %
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -95,30 +92,7 @@ J=s+J;
 %               and Theta2_grad from Part 2.
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
-
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
